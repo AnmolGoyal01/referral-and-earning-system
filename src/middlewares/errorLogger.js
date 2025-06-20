e6f5c9e74f5be023b2750b119cd366e72e1097d8
anmolgoyal1212 <anmolgoyal_ee21b6_59@dtu.ac.in>
2025-06-21 03:24:39 +0530

    fix typo

--- src/middlewares/errorLogger.js
+++ src/middlewares/errorLogger.js
@@ -5,7 +5,7 @@ export const errorLogger = (err, req, res, next) => {
         `[❌ API ERROR] ${req.method} ${req.originalUrl} - ${err.message}`,
         err
     );
-    res.status(err.status || 500).json({
+    res.status(err.statusCode || 500).json({
         success: false,
         message: err.message || "Internal Server Error",
     });
